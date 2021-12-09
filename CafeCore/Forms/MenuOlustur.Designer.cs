bf56de18b1af3600a09e7de47b225207c7828fd3
sefaa-dev <sefayilmaz582@gmail.com>
2021-12-09 11:09:58 +0300

    cafecore

--- CafeCore/Forms/MenuOlustur.Designer.cs
+++ CafeCore/Forms/MenuOlustur.Designer.cs
@@ -37,6 +37,12 @@ namespace CafeCore.Forms
             this.btnUrunKaydet = new System.Windows.Forms.Button();
             this.lvKategoriler = new System.Windows.Forms.ListView();
             this.lvUrunler = new System.Windows.Forms.ListView();
+            this.cmbKategoriler = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtUrunFiyat = new System.Windows.Forms.TextBox();
+            this.btnKategoriGuncelle = new System.Windows.Forms.Button();
+            this.btnUrunGüncelle = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -57,9 +63,9 @@ namespace CafeCore.Forms
             // 
             // btnKategoriKaydet
             // 
-            this.btnKategoriKaydet.Location = new System.Drawing.Point(102, 47);
+            this.btnKategoriKaydet.Location = new System.Drawing.Point(46, 345);
             this.btnKategoriKaydet.Name = "btnKategoriKaydet";
-            this.btnKategoriKaydet.Size = new System.Drawing.Size(109, 55);
+            this.btnKategoriKaydet.Size = new System.Drawing.Size(72, 57);
             this.btnKategoriKaydet.TabIndex = 2;
             this.btnKategoriKaydet.Text = "Kategori Kaydet";
             this.btnKategoriKaydet.UseVisualStyleBackColor = true;
@@ -68,7 +74,7 @@ namespace CafeCore.Forms
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(387, 21);
+            this.label2.Location = new System.Drawing.Point(575, 65);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(54, 15);
             this.label2.TabIndex = 0;
@@ -76,14 +82,14 @@ namespace CafeCore.Forms
             // 
             // txtUrunAdi
             // 
-            this.txtUrunAdi.Location = new System.Drawing.Point(465, 18);
+            this.txtUrunAdi.Location = new System.Drawing.Point(644, 62);
             this.txtUrunAdi.Name = "txtUrunAdi";
             this.txtUrunAdi.Size = new System.Drawing.Size(145, 23);
             this.txtUrunAdi.TabIndex = 1;
             // 
             // btnUrunKaydet
             // 
-            this.btnUrunKaydet.Location = new System.Drawing.Point(477, 47);
+            this.btnUrunKaydet.Location = new System.Drawing.Point(834, 131);
             this.btnUrunKaydet.Name = "btnUrunKaydet";
             this.btnUrunKaydet.Size = new System.Drawing.Size(109, 55);
             this.btnUrunKaydet.TabIndex = 2;
@@ -94,9 +100,9 @@ namespace CafeCore.Forms
             // lvKategoriler
             // 
             this.lvKategoriler.HideSelection = false;
-            this.lvKategoriler.Location = new System.Drawing.Point(23, 119);
+            this.lvKategoriler.Location = new System.Drawing.Point(31, 57);
             this.lvKategoriler.Name = "lvKategoriler";
-            this.lvKategoriler.Size = new System.Drawing.Size(280, 262);
+            this.lvKategoriler.Size = new System.Drawing.Size(295, 262);
             this.lvKategoriler.TabIndex = 3;
             this.lvKategoriler.UseCompatibleStateImageBehavior = false;
             this.lvKategoriler.SelectedIndexChanged += new System.EventHandler(this.lvKategoriler_SelectedIndexChanged);
@@ -104,22 +110,82 @@ namespace CafeCore.Forms
             // lvUrunler
             // 
             this.lvUrunler.HideSelection = false;
-            this.lvUrunler.Location = new System.Drawing.Point(400, 119);
+            this.lvUrunler.Location = new System.Drawing.Point(535, 131);
             this.lvUrunler.Name = "lvUrunler";
             this.lvUrunler.Size = new System.Drawing.Size(270, 262);
             this.lvUrunler.TabIndex = 3;
             this.lvUrunler.UseCompatibleStateImageBehavior = false;
             this.lvUrunler.SelectedIndexChanged += new System.EventHandler(this.lvUrunler_SelectedIndexChanged);
             // 
+            // cmbKategoriler
+            // 
+            this.cmbKategoriler.FormattingEnabled = true;
+            this.cmbKategoriler.Location = new System.Drawing.Point(644, 15);
+            this.cmbKategoriler.Name = "cmbKategoriler";
+            this.cmbKategoriler.Size = new System.Drawing.Size(145, 23);
+            this.cmbKategoriler.TabIndex = 4;
+            this.cmbKategoriler.SelectedIndexChanged += new System.EventHandler(this.cmbKategoriler_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(565, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Kategoriler";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(565, 105);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 15);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Ürün Fiyatı";
+            // 
+            // txtUrunFiyat
+            // 
+            this.txtUrunFiyat.Location = new System.Drawing.Point(644, 102);
+            this.txtUrunFiyat.Name = "txtUrunFiyat";
+            this.txtUrunFiyat.Size = new System.Drawing.Size(145, 23);
+            this.txtUrunFiyat.TabIndex = 1;
+            // 
+            // btnKategoriGuncelle
+            // 
+            this.btnKategoriGuncelle.Location = new System.Drawing.Point(232, 345);
+            this.btnKategoriGuncelle.Name = "btnKategoriGuncelle";
+            this.btnKategoriGuncelle.Size = new System.Drawing.Size(75, 56);
+            this.btnKategoriGuncelle.TabIndex = 6;
+            this.btnKategoriGuncelle.Text = "Kategori Güncelle";
+            this.btnKategoriGuncelle.UseVisualStyleBackColor = true;
+            this.btnKategoriGuncelle.Click += new System.EventHandler(this.btnKategoriGuncelle_Click);
+            // 
+            // btnUrunGüncelle
+            // 
+            this.btnUrunGüncelle.Location = new System.Drawing.Point(834, 203);
+            this.btnUrunGüncelle.Name = "btnUrunGüncelle";
+            this.btnUrunGüncelle.Size = new System.Drawing.Size(109, 58);
+            this.btnUrunGüncelle.TabIndex = 7;
+            this.btnUrunGüncelle.Text = "Ürün Güncelle";
+            this.btnUrunGüncelle.UseVisualStyleBackColor = true;
+            this.btnUrunGüncelle.Click += new System.EventHandler(this.btnUrunGüncelle_Click);
+            // 
             // MenuOlustur
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1208, 450);
+            this.Controls.Add(this.btnUrunGüncelle);
+            this.Controls.Add(this.btnKategoriGuncelle);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cmbKategoriler);
             this.Controls.Add(this.lvUrunler);
             this.Controls.Add(this.lvKategoriler);
             this.Controls.Add(this.btnUrunKaydet);
+            this.Controls.Add(this.txtUrunFiyat);
             this.Controls.Add(this.txtUrunAdi);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.btnKategoriKaydet);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.txtKategoriAdi);
@@ -142,5 +208,11 @@ namespace CafeCore.Forms
         private System.Windows.Forms.Button btnUrunKaydet;
         private System.Windows.Forms.ListView lvKategoriler;
         private System.Windows.Forms.ListView lvUrunler;
+        private System.Windows.Forms.ComboBox cmbKategoriler;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtUrunFiyat;
+        private System.Windows.Forms.Button btnKategoriGuncelle;
+        private System.Windows.Forms.Button btnUrunGüncelle;
     }
 }
--- CafeCore/Forms/MenuOlustur.cs
+++ CafeCore/Forms/MenuOlustur.cs
@@ -1,11 +1,13 @@
 ﻿using CafeCore.Data;
 using CafeCore.Model;
 using CafeCore.Repository;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,9 +26,12 @@ namespace CafeCore.Forms
 
         private void MenuOlustur_Load(object sender, EventArgs e)
         {
-            KategoriListeDoldur();
-            UrunListeDoldur();
+            ListeyiDoldur();
 
+        }
+        private void txtUrunFiyat_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar) && e.KeyChar != '.';
         }
 
         private void KategoriListeDoldur()
@@ -36,20 +41,23 @@ namespace CafeCore.Forms
             lvKategoriler.MultiSelect = false;
             lvKategoriler.View = View.Details;
             lvKategoriler.FullRowSelect = true;
-            lvKategoriler.Columns.Add("Kategori İsmi");
-           
+            lvKategoriler.Columns.Add("Kategori İsmi");          
+            lvKategoriler.Columns.Add("Ürün Sayısı");
 
-            var queryKategori = _dbContext.Kategoriler.OrderBy(x => x.Ad).ToList();
+
+            var queryKategori = _dbContext.Kategoriler.Include(x=>x.Urunler).OrderBy(x => x.Ad).ToList();
+            
             foreach (var item in queryKategori)
             {
                 ListViewItem viewItem = new ListViewItem(item.Ad);
                 viewItem.SubItems.Add(item.Ad);
-                
+                viewItem.SubItems.Add(item.Urunler.Count.ToString());                
                 viewItem.Tag = item;
                 lvKategoriler.Items.Add(viewItem);
             }
             lvKategoriler.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
         }
+        
 
         private void btnKategoriKaydet_Click(object sender, EventArgs e)
         {
@@ -71,7 +79,7 @@ namespace CafeCore.Forms
             }
             finally
             {
-                KategoriListeDoldur();
+                ListeyiDoldur();
             }
         }
 
@@ -87,34 +95,46 @@ namespace CafeCore.Forms
 
         private void UrunListeDoldur()
         {
+            cmbKategoriler.DataSource = _dbContext.Kategoriler.OrderBy(x => x.Ad).ToList();
+            cmbKategoriler.DisplayMember = "Ad";
+            
+            
             lvUrunler.Columns.Clear();
             lvUrunler.Items.Clear();
             lvUrunler.MultiSelect = false;
             lvUrunler.View = View.Details;
             lvUrunler.FullRowSelect = true;
-            lvUrunler.Columns.Add("Ürün İsmi");
+            lvUrunler.Columns.Add("Kategori");
+            lvUrunler.Columns.Add("Ürün Adı");
+            lvUrunler.Columns.Add("Fiyat");
 
 
-            var queryUrun = _dbContext.Urunler.OrderBy(x => x.Ad).ToList();
+            var queryUrun = _dbContext.Urunler.OrderBy(x => x.Kategori.Ad).ToList();
             foreach (var item in queryUrun)
             {
-                ListViewItem viewItem = new ListViewItem(item.Ad);
-                viewItem.SubItems.Add(item.Ad);
-
+                ListViewItem viewItem = new ListViewItem(item.Kategori.Ad);
+                viewItem.SubItems.Add(item.Ad.ToString());
+                viewItem.SubItems.Add($"{item.Fiyat:c2}");
                 viewItem.Tag = item;
                 lvUrunler.Items.Add(viewItem);
             }
            lvUrunler.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
         }
-
+        private void ListeyiDoldur()
+        {
+            KategoriListeDoldur();
+            UrunListeDoldur();
+        }
         private void btnUrunKaydet_Click(object sender, EventArgs e)
         {
             try
             {
                 var yeniUrun = new Urun
                 {
-                    Ad = txtUrunAdi.Text
-
+                    Kategori = cmbKategoriler.SelectedItem as Kategori,
+                    Ad = txtUrunAdi.Text,
+                    Fiyat = Decimal.Parse(txtUrunFiyat.Text, NumberStyles.AllowDecimalPoint, CultureInfo.InvariantCulture)
+                   
                 };
 
                 _dbContext.Urunler.Add(yeniUrun);
@@ -127,7 +147,7 @@ namespace CafeCore.Forms
             }
             finally
             {
-                UrunListeDoldur();
+                ListeyiDoldur();
             }
         }
 
@@ -139,6 +159,63 @@ namespace CafeCore.Forms
             _seciliUrun = lvUrunler.SelectedItems[0].Tag as Urun;
 
             txtUrunAdi.Text = _seciliUrun.Ad;
+            txtUrunFiyat.Text = _seciliUrun.Fiyat.ToString(CultureInfo.InvariantCulture);
+            cmbKategoriler.SelectedItem = _seciliUrun.Kategori;
+        }
+
+        private void btnKategoriGuncelle_Click(object sender, EventArgs e)
+        {
+            if (lvKategoriler.SelectedItems.Count == 0) return;
+            _seciliKategori = lvKategoriler.SelectedItems[0].Tag as Kategori;
+
+            try
+            {
+                _seciliKategori.Ad = txtKategoriAdi.Text;
+
+                _dbContext.Kategoriler.Update(_seciliKategori);
+                _dbContext.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+                _dbContext = new CafeContext();
+            }
+            finally
+            {
+                ListeyiDoldur();
+            }
+        }
+
+        private void btnUrunGüncelle_Click(object sender, EventArgs e)
+        {
+            if (lvUrunler.SelectedItems.Count == 0) return;
+            _seciliUrun = lvUrunler.SelectedItems[0].Tag as Urun;
+
+            try
+            {
+                _seciliUrun.Kategori = cmbKategoriler.SelectedItem as Kategori;
+                _seciliUrun.Ad = txtUrunAdi.Text;
+                _seciliUrun.Fiyat = decimal.Parse(txtUrunFiyat.Text, NumberStyles.AllowDecimalPoint, CultureInfo.InvariantCulture);
+
+                _dbContext.Urunler.Update(_seciliUrun);
+                _dbContext.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                _dbContext = new CafeContext();
+            }
+            finally
+            {
+                ListeyiDoldur();
+            }
+        }
+
+        private void cmbKategoriler_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cmbKategoriler.SelectedItem != null) return;
+            _seciliKategori = cmbKategoriler.SelectedItem as Kategori;
         }
     }
     }
